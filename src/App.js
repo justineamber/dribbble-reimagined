56b57c8ee1bc64b28346fcae31a4ed24a3c11352
Justine Hill <justinehill@Justines-MacBook-Pro.local>
2020-01-02 09:25:13 +0100

    toolbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import Toolbar from './components/Toolbar/Toolbar';
 
 function App() {
   return (
+    <>
       <Header />
+      <Toolbar />
+    </>
   );
 }
 
