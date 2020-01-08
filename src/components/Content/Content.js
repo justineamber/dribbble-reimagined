e35f0e45379f902b4a9dd0214b5f0fb7edf579fb
Justine Hill <justinehill@Justines-MacBook-Pro.local>
2020-01-08 11:21:53 +0100

    card content responsive edits

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -111,21 +111,6 @@ function Content() {
           </div>
         </div>
       </div>
-      <div className="card">
-        <div className="card_image-container">
-          <img src="https://images.unsplash.com/photo-1520190282873-afe1285c9a2a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1200&q=80" alt="cabin ontop of mountain"
-          />
-        </div>
-        <div className="card_content">
-          <div className="card_info">
-            <img src="/afterglow-logo.png" alt="designer logo" className="designer-logo"></img>
-            <p className="text--medium">Outsourced<sup className="company-info">PRO</sup></p>
-                <p className="card_engagement text--medium"><a href="#"><Eye /></a>4910</p>
-                <p className="card_engagement text--medium"><a href="#"><MessageCircle /></a>27</p>
-                <p className="card_engagement text--medium"><a href="#"><Heart /></a>543</p>
-          </div>
-        </div>
-      </div>
     </section>
   </main>
   );
